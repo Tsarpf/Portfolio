f8f6222fa25112fc26cd14b7db2e6649ea75351c
Teemu Sarapisto <tsarpf@gmail.com>
2014-11-16 14:56:21 -0500

    Fixed paths

--- public/js/views/entryView.js
+++ public/js/views/entryView.js
@@ -5,7 +5,7 @@ var EntryView = Backbone.View.extend({
     render: function() {
         var self = this;
 
-        $.get('templates/entry.html', function(data) {
+        $.get('/templates/entry.html', function(data) {
             var template = _.template(data);
             self.$el.html(template({entry: self.model}));
             console.log(self.el);
--- server/routes.js
+++ server/routes.js
@@ -2,10 +2,11 @@ module.exports = function(app) {
 
     var serveIndex = function(res, cb) {
         var obj = {
+            root: './public'
             /*TODO: add cache headers
              * And render pages on the server instead of ajaxing them*/
         };
-        res.sendFile('/public/index.html', obj, cb);
+        res.sendFile('/index.html', obj, cb);
 
     }
 
